facf35f645067dc699f37d74baaa2f533f693ac2
Alec S <linuxbash8@gmail.com>
2015-05-23 19:35:05 -0500

    Server working

--- Server.hs
+++ Server.hs
@@ -1,10 +1,77 @@
--- | Runner for Chat server. We put Main separately so that we can keep chat as
--- a library for testing.
+{- Alec Snyder
+- lab 2 chat server program
+- following networking from O'sullivan's Real World Haskell
+-}
 module Main (main) where
+
+import Network.Socket
+import Network.BSD
+import Control.Concurrent
+import Control.Concurrent.MVar
+import System.IO
+import System.Environment
 
 import Chat
 
--- | Run our chat server.
+--Main: Set up socket and start forking
 main :: IO ()
-main = chat
+main = do
+    portStr <- getEnv "CHAT_SERVER_PORT"
+    let port = read portStr :: Int
+    --now we grab our address information
+    putStrLn "Grabbing server information"
+    addrinfo <- getAddrInfo 
+                (Just (defaultHints {addrFlags = [AI_PASSIVE]})) 
+                Nothing 
+                (Just portStr)
+    let address = head addrinfo
+    
+    --set up TCP (Stream) socket
+    putStrLn "creating socket"
+    sock <- socket (addrFamily address) Stream defaultProtocol
+    setSocketOption sock ReuseAddr 1
+    
+    --attempt to bind
+    putStrLn "binding to socket"
+    bindSocket sock (addrAddress address)
+    
+    --listen on socket
+    listen sock 10
+    
+    var <- newMVar []
+    
+    --call function to accept connections
+    acceptCons sock var
 
+--acceptCons: loops and accepts connections and then spawns child procs
+--to handle clients
+
+acceptCons :: Socket -> MVar [User] -> IO ()
+acceptCons sock var = do
+    (conn, client) <- accept sock
+    putStrLn $ "Client connected from ip: " ++ (show client) 
+    forkIO (handleClient conn var)
+    acceptCons sock var
+
+handleClient :: Socket -> MVar [User] -> IO ()
+handleClient sock var= do
+    hand <- socketToHandle sock ReadWriteMode
+    hSetBuffering hand LineBuffering
+    newUser <- joinUser hand var
+    chat newUser var
+
+chat :: User -> MVar [User] -> IO ()
+chat use var = do
+    let hand = han use
+    end <- hIsEOF hand
+    if end
+        then do
+            putStrLn "Closing client"
+            userQuit use var
+            hClose hand
+        else do
+            recv <- hGetLine hand
+            --putStrLn $ "received: " ++ recv
+            userList <- readMVar var
+            broadcastMessage use ((show use) ++ ": " ++ recv) userList
+            chat use var
--- src/Chat.hs
+++ src/Chat.hs
@@ -1,7 +1,53 @@
--- | CS240h Lab 2 Chat Server
-module Chat (chat) where
+{- Alec Snyder
+- chatServer Library
+-}
 
--- | Chat server entry point.
-chat :: IO ()
-chat = return ()
+module Chat (User(User),han,joinUser,broadcastMessage,userQuit) where
 
+import System.IO
+import Control.Monad
+import Control.Concurrent
+import Control.Concurrent.MVar
+import Control.Exception.Base
+
+data User = User {ident :: Int
+                 ,han :: Handle}
+    deriving(Eq)
+
+instance Show User where
+    show u = show (ident u)
+
+writeMessage :: String -> User -> IO ()
+writeMessage str u = hPutStrLn (han u) str
+
+joinUser :: Handle -> MVar [User] -> IO User
+joinUser han var = do
+    ls <- takeMVar var
+    let biggestUser = grabUserName ls
+    u <- evaluate $ User (biggestUser + 1) han
+    newList <- evaluate (u:ls)
+    mapM_ (writeMessage ((show u) ++ " has joined")) newList
+    putMVar var newList
+    return u where
+        grabUserName [] = 0
+        grabUserName (x:xs) = ident x
+
+--broadcastMessage: broadcasts a message from user u to every user but u
+broadcastMessage :: User -> String -> [User] -> IO ()
+broadcastMessage u str (x:xs)
+    | x == u = mapM_ (writeMessage (str)) xs
+    | otherwise = do
+        writeMessage ((show u) ++ ": " ++ str) x
+        broadcastMessage u str xs
+
+userQuit :: User -> MVar [User] -> IO ()
+userQuit u var = do
+    ls <- takeMVar var
+    broadcastMessage u ((show u) ++ " has left") ls
+    newList <- evaluate $ remUser u ls
+    putMVar var newList
+    return () where
+        remUser user (x:xs)
+            | user == x = xs
+            | otherwise = x : remUser user xs
+    
